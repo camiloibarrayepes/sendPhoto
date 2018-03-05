4e6896e8e8040706b10ba156e6487f71f814cdb4
camilo ibarra <camilo.ibarray@gmail.com>
2018-03-04 23:33:35 -0500

    First commit, code complete, exmaple

---
+++ app/src/main/java/com/example/camiloandresibarrayepes/pruebafoto3/MainActivity.java
@@ -0,0 +1,199 @@
+package com.example.camiloandresibarrayepes.pruebafoto3;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+
+import com.kosalgeek.android.photoutil.CameraPhoto;
+import com.kosalgeek.android.photoutil.GalleryPhoto;
+import com.kosalgeek.android.photoutil.ImageBase64;
+import com.kosalgeek.android.photoutil.ImageLoader;
+import com.kosalgeek.asynctask.AsyncResponse;
+import com.kosalgeek.asynctask.EachExceptionsHandler;
+import com.kosalgeek.asynctask.PostResponseAsyncTask;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getName();
+
+    private ProgressDialog progressDialog;
+
+    ImageView ivCamera, ivGallery, ivImage, ivUpload;
+
+    CameraPhoto cameraPhoto;
+
+    GalleryPhoto galleryPhoto;
+
+    final int CAMERA_REQUEST = 1100;
+    final int GALLERY_REQUEST = 2200;
+
+    String selectedPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(this);
+
+
+        cameraPhoto = new CameraPhoto(getApplicationContext());
+        galleryPhoto = new GalleryPhoto(getApplicationContext());
+
+        ivCamera = (ImageView) findViewById(R.id.ivCamera);
+        ivGallery = (ImageView) findViewById(R.id.ivGallery);
+        ivImage = (ImageView) findViewById(R.id.ivImage);
+        ivUpload = (ImageView) findViewById(R.id.ivUpload);
+
+        ivCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    startActivityForResult(cameraPhoto.takePhotoIntent(), CAMERA_REQUEST);
+                    cameraPhoto.addToGallery();
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SALIO ALGO MAL", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        ivGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(galleryPhoto.openGalleryIntent(), GALLERY_REQUEST);
+            }
+        });
+
+
+        ivUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(selectedPhoto.equals("") || selectedPhoto == null){
+                    Toast.makeText(getApplicationContext(),
+                            "NO HAY IMAGEN", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                try {
+                    Bitmap bitmap = ImageLoader.init().from(selectedPhoto).requestSize(1024, 1024).getBitmap();
+                    String encodedImage = ImageBase64.encode(bitmap);
+                    Log.d(TAG, encodedImage);
+
+                    //SEND TO PHP SCRIPT
+                    HashMap<String, String> postData = new HashMap<String, String>();
+                    postData.put("image", encodedImage);
+
+
+                    PostResponseAsyncTask task = new PostResponseAsyncTask(MainActivity.this, postData, new AsyncResponse() {
+                        @Override
+                        public void processFinish(String s) {
+                            Log.d(TAG,s);
+
+                            if(s.contains("upload_success")){
+                                Toast.makeText(getApplicationContext(),
+                                        "ENVIADO CON EXITO", Toast.LENGTH_SHORT).show();
+                            }else{
+                                //TODO
+                                //Change text, error por exito
+                                Toast.makeText(getApplicationContext(),
+                                        "Imagen enviada", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+                    task.execute("https://addaprueba.000webhostapp.com/connection.php");
+                    progressDialog.setMessage("Enviando tu denuncia, por favor espera, puede tardar unos segundos...");
+                    progressDialog.show();
+                    task.setEachExceptionsHandler(new EachExceptionsHandler() {
+                        @Override
+                        public void handleIOException(IOException e) {
+                            Toast.makeText(getApplicationContext(),
+                                    "ERROR CONECTANDO", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void handleMalformedURLException(MalformedURLException e) {
+                            Toast.makeText(getApplicationContext(),
+                                    "URL ERROR", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void handleProtocolException(ProtocolException e) {
+                            Toast.makeText(getApplicationContext(),
+                                    "ERROR PROTOCOLO", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void handleUnsupportedEncodingException(UnsupportedEncodingException e) {
+                            Toast.makeText(getApplicationContext(),
+                                    "ENCODING ERROR", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SALIO ALGO MAL CODIFICANDO LAS FOTOS", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK){
+            if(requestCode == CAMERA_REQUEST){
+                String photoPath = cameraPhoto.getPhotoPath();
+                selectedPhoto = photoPath;
+                try {
+                    Bitmap bitmap = ImageLoader.init().from(photoPath).requestSize(512, 512).getBitmap();
+                    ivImage.setImageBitmap(getRotateBitmap(bitmap, 90));
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SALIO ALGO MAL SUBIENDO LAS FOTOS", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            else if(requestCode == GALLERY_REQUEST){
+                Uri uri = data.getData();
+                galleryPhoto.setPhotoUri(uri);
+                String photoPath = galleryPhoto.getPath();
+                selectedPhoto = photoPath;
+                try {
+                    Bitmap bitmap = ImageLoader.init().from(photoPath).requestSize(512, 512).getBitmap();
+                    ivImage.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SALIO ALGO MAL ESCOGIENDO LAS FOTOS", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+
+    private Bitmap getRotateBitmap(Bitmap source, int angle){
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        Bitmap bitmap1 = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+        return bitmap1;
+    }
+}
