9d2eab751073a403c595034efd74f94086ddc394
camilo ibarra <camilo.ibarray@gmail.com>
2018-03-08 23:43:20 -0500

    refactor webview

--- app/src/main/java/com/example/camiloandresibarrayepes/pruebafoto3/info_adda.java
+++ app/src/main/java/com/example/camiloandresibarrayepes/pruebafoto3/info_adda.java
@@ -2,6 +2,7 @@ package com.example.camiloandresibarrayepes.pruebafoto3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,18 +11,36 @@ public class info_adda extends AppCompatActivity {
 
 
     String url = "http:www.yamgo.com.co/adda/ver.php";
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_adda);
 
-        WebView web = (WebView)findViewById(R.id.wiew);
-        web.setWebViewClient(new MyWebViewClient());
-        WebSettings setting = web.getSettings();
+        mWebView = (WebView) findViewById(R.id.wiew);
+        mWebView.setWebViewClient(new MyWebViewClient());
+        WebSettings setting = mWebView.getSettings();
         setting.setJavaScriptEnabled(true);
-        web.loadUrl(url);
+        mWebView.loadUrl(url);
 
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (mWebView.canGoBack()) {
+                        mWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     private class MyWebViewClient extends WebViewClient{
